e112544dbcff921de33864a9a10a71ccf104387b
Andrea <andryx1994@gmail.com>
2020-07-03 20:20:19 +0200

    Versione 1.1.2
    Sistemato codice JS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var nomePlaylist = prompt("Inserisci nome playlist");
-alert("Ho aggiunto la playlist " + nomePlaylist);
-
-document.getElementById("playlist-js").innerHTML= nomePlaylist;
+// var nomePlaylist = prompt("Inserisci nome playlist");
+// alert("Ho aggiunto la playlist " + nomePlaylist);
+//
+// document.getElementById("playlist-js").innerHTML= nomePlaylist;
